ab4bf8f8c658fe3aa2f28561a468fcb4e53118cc
matiasealianelli <hola.matiasalianelli@gmail.com>
2025-02-05 17:12:50 -0300

    Agregue estructura jsx del Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,80 @@
-import '/src/styles/Header.css';
+import "/src/styles/Header.css";
 
 export default function Header() {
-    return (
-      <>
-      
-      </>
-    )
-  }
+  let lastScrollTop = 0;
+  window.addEventListener('scroll', function () {
+    let st = document.documentElement.scrollTop;
+    if (st === 0) {
+      // Desplazamiento hacia abajo
+      document.querySelector('header').style.backgroundColor = 'transparent';
+    }
+    else if (st < lastScrollTop) {
+      // Desplazamiento hacia arriba
+      document.querySelector('header').style.backgroundColor = '#ffffff';
+    }else if (st > lastScrollTop) {
+      document.querySelector('header').style.backgroundColor = '#ffffff';
+      document.querySelector('header').style.transition = 'all 0.3s linear';
+    }
+    lastScrollTop = st <= 0 ? 0 : st; // Para evitar que el valor sea negativo
+});
+  return (
+    <>
+      <header>
+        <img className="logo" src="./assets/img/logo.svg" alt="logo manage" />
+        <img
+          className="iconMenu"
+          src="./assets/img/icon-hamburger.svg"
+          alt="menu"
+          onClick={() => {
+            const iconMenu = document.querySelector(".iconMenu");
+            const iconClose = document.querySelector(".iconClose");
+            const nav = document.querySelector("nav");
+            // const btn = document.querySelector(".btnStarted");
+            if (window.matchMedia("(max-width: 767px)").matches) {
+              iconMenu.addEventListener("click", () => {
+                nav.classList.add("active");
+                iconMenu.style.display = "none";
+                iconClose.style.display = "block";
+              });
+              iconClose.addEventListener("click", () => {
+                nav.classList.remove("active");
+                iconClose.style.display = "none";
+                iconMenu.style.display = "block";
+              });
+              document.querySelectorAll("nav a").forEach((link) => {
+                link.addEventListener("click", () => {
+                  nav.classList.remove("active");
+                  iconClose.style.display = "none";
+                  iconMenu.style.display = "block";
+                });
+              });
+            }
+          }}
+        />
+        <img
+          className="iconClose"
+          src="./assets/img/icon-close.svg"
+          alt="close-menu"
+        />
+        <nav>
+          <a href="#" className="link">
+            Pricing
+          </a>
+          <a href="#" className="link">
+            Product
+          </a>
+          <a href="#" className="link">
+            About Us
+          </a>
+          <a href="#" className="link">
+            Careers
+          </a>
+          <a href="#" className="link">
+            Community
+          </a>
+        </nav>
+        <button className="btnStarted">Get Started</button>
+      </header>
+    </>
+  );
+}
