3670d8c5c80b3fbdcdaad52fda9c94bb96f6e044
matiasealianelli <hola.matiasalianelli@gmail.com>
2025-02-06 13:43:21 -0300

    Agregue estructura jsx del Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,25 @@
+import RedesSociales from './RedesSociales.jsx';
 import '/src/styles/Hero.css';
 
 export default function Hero() {
     return (
       <>
-      
+        <section className='hero' id='hero'>
+        <div className='contInfoHero'>
+          <h1 className='title'>
+            Bring everyone together to build better products.
+          </h1>
+          <p className='description'>
+            Manage makes it simple for software teams to plan day-to-day tasks while keeping the larger team goals in view.
+          </p>
+          <button className='btnStarted btnHero'>
+            Get Started
+            </button>
+            <RedesSociales />
+        </div>
+       <img className='graph' src='/assets/img/illustration-intro.svg' alt='illustration-working'/>
+        
+        </section>
       </>
     )
   }
