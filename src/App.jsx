0f2c94329584543f3fcc94556362db6e07f001d1
matiasealianelli <hola.matiasalianelli@gmail.com>
2025-02-16 21:50:26 -0300

    Modifique el App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,36 @@
-//import { useState } from 'react'
-import './App.css'
-import Header from './components/Header'
-import Hero from './components/Hero'
-import Service from './components/Service' 
-import Reviews from './components/Reviews'
-import Footer from './components/Footer'
-import './index.css'
+import "./App.css";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Service from "./components/Service";
+import Reviews from "./components/Reviews component/Reviews.jsx";
+import Footer from "./components/Footer";
+import "./index.css";
 
 export default function App() {
   return (
     <>
+      <Header />
       <div className="contSection">
-        <Header />
+        <img
+          src="./assets/img/bg-tablet-pattern.svg"
+          className="bg-img-top"
+          alt=""
+        />
         <Hero />
         <Service />
+        <img
+          src="./assets/img/bg-tablet-pattern.svg"
+          className="bg-img-bottom"
+          alt=""
+        />
         <Reviews />
-        <Footer />
       </div>
-      
+      {/* "cta" = Call To Action */}
+      <div className="cont-cta">
+        <h1 className="title-cta">Simplify how your team works today.</h1>
+        <button className="btn-cta">Get Started</button>
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
